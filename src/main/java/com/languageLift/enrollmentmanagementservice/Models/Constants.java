a9546f70b0eca0c8d9deb762e1adbe3f28e0e153
manavumd <manavg@umd.edu>
2023-11-25 15:02:02 -0500

    changed ip address for aws

--- src/main/java/com/languageLift/enrollmentmanagementservice/Models/Constants.java
+++ src/main/java/com/languageLift/enrollmentmanagementservice/Models/Constants.java
@@ -2,5 +2,5 @@ package com.languageLift.enrollmentmanagementservice.Models;
 
 public class Constants {
 
-    public static final String AWS_IP = "35.171.189.199";
+    public static final String AWS_IP = "3.92.127.22";
 }
